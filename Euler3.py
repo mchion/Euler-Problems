66bcf1be387f0814d5f4ac82cb246fdd69d2ba74
Michael Chion <michael.chion@gmail.com>
2019-08-19 15:00:54 -0700

    Update Euler3.py

--- Euler3.py
+++ Euler3.py
@@ -1,7 +1,7 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 # What is the largest prime factor of the number 600851475143
 
-n = 25
+n = 600851475143
 i = 2
 
 while i*i <=n:   #a prime factor is always less than or equal to the square root of the number
